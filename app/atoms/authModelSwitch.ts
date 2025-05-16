27a24b265e373819d6e40be03a1fe981029a0535
Himanshu Mishra <mickeysingh.r4@gmail.com>
2025-05-16 22:05:29 +0530

    removed recoil and used fs with require

--- app/atoms/authModelSwitch.ts
+++
@@ -1,5 +0,0 @@
-import { atom } from "recoil";
-export const authModelSwitch = atom({
-  key: "authModelSwitch",
-  default: false,
-});
--- problems/filter.js
+++ problems/filter.js
@@ -1,4 +1,5 @@
-import fs from "fs";
+// import fs from "fs";
+const fs = require("fs");
 import { PrismaClient } from "../lib/generated/prisma/client.js";
 const prisma = new PrismaClient();
 async function importProblems() {
