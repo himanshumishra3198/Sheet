be8ffe59e0317c65d9c8652f519d347fe0ee296a
Himanshu Mishra <mickeysingh.r4@gmail.com>
2025-05-17 15:31:06 +0530

    fixing production issue

--- auth.ts
+++ auth.ts
@@ -4,28 +4,30 @@ import { prismaClient } from "./prisma/src";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  trustHost: true, // Only needed if not using Vercel/AWS
+  basePath: "/api/auth", // Explicit base path
   session: {
     strategy: "jwt",
   },
-  debug: true,
+  secret: process.env.AUTH_SECRET, // Must be set in production
+  debug: process.env.NODE_ENV !== "production",
   callbacks: {
     async jwt({ token, user }) {
-      if (user && user.email) {
+      if (user?.email) {
         const dbUser = await prismaClient.user.findUnique({
           where: { email: user.email },
         });
-        if (!dbUser) {
-          const newUser = await prismaClient.user.create({
-            data: {
-              email: user.email,
-              name: user.name,
-              avatar: user.image ?? "",
-            },
-          });
-          token.id = newUser.id;
-        } else {
-          token.id = dbUser.id;
-        }
+        token.id =
+          dbUser?.id ||
+          (
+            await prismaClient.user.create({
+              data: {
+                email: user.email,
+                name: user.name,
+                avatar: user.image ?? "",
+              },
+            })
+          ).id;
       }
       return token;
     },
@@ -34,6 +36,18 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         session.user.id = token.id as string;
       }
       return session;
+    },
+  },
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: process.env.NODE_ENV === "production",
+        domain: process.env.NODE_ENV === "production" ? ".hm0.org" : undefined,
+      },
     },
   },
 });
