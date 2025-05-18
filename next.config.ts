042c5340aa492dde7a26a6fc1fabfb4bcd79c9f5
Himanshu Mishra <mickeysingh.r4@gmail.com>
2025-05-18 19:11:18 +0530

    explicitly defining dburl

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    DATABASE_URL:
+      "postgresql://postgres:admin@localhost:5432/mydb?schema=public",
+  },
 };
 
 export default nextConfig;
