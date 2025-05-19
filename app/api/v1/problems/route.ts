18873155593b03a3642ecee2f400153e93a60f70
Himanshu Mishra <mickeysingh.r4@gmail.com>
2025-05-20 02:23:26 +0530

    fixing

---
+++ app/api/v1/problems/route.ts
@@ -0,0 +1,22 @@
+// import { prismaClient } from "@/prisma/src";
+import { PrismaClient } from "@/lib/generated/prisma";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const prismaClient = new PrismaClient();
+  try {
+    const data = await prismaClient.problem.findMany();
+    await prismaClient.$disconnect();
+    console.log("data", data);
+    return new Response(JSON.stringify({ data }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Failed to fetch problems:", error);
+    await prismaClient.$disconnect();
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+    });
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
-import { prismaClient } from "@/prisma/src";
+"use client";
 import { ProblemType } from "./utils/ProblemType";
 import App from "./App";
+import { useEffect, useState } from "react";
 
-export default async function Home() {
-  const data: ProblemType[] = await prismaClient.problem.findMany();
+export default function Home() {
+  // const data: ProblemType[] = await prismaClient.problem.findMany();
+  const [problems, setProblems] = useState<ProblemType[]>([]);
+  useEffect(() => {
+    const fetchProblems = async () => {
+      const res = await fetch("/api/v1/problems");
+      if (res.ok) {
+        const data = await res.json();
+
+        setProblems(data.data);
+      }
+    };
+    fetchProblems();
+  }, []);
 
   // await prismaClient.$disconnect().catch((e) => {
   //   console.error("Error disconnecting Prisma:", e);
   // });
   // console.log(data);
-  return <App problems={data} />;
+  if (!problems) {
+    return <div>Loading...</div>;
+  }
+  return <App problems={problems} />;
 }
